af164bb8578b1279c3b6f6d6c8efab5a00435e0b
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-31 02:34:39 +0300

    check2

--- server.js
+++ server.js
@@ -28,19 +28,33 @@ server.get('/getCity',getCityHandler);
 
 async function getCityHandler(req,res){
     const searchQ = req.query.cityName;
-    console.log(searchQ);
+    // console.log(searchQ);
     const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQ},NC&key=${process.env.key}`;
-    console.log(URL);
+    // console.log(URL);
 
 
 try
 {
     let apiResult = await axios.get(URL);
-    res.send(apiResult.data);
+    // res.send(apiResult.data);
+    let cityArray = apiResult.data.data.map(item =>{
+        
+        // console.log(item.valid_date);
+        return new Weather(item);
+        // res.send(item);
+    })
+    res.send(cityArray);
 }
 catch{
 
 }
+}
+
+class Weather {
+    constructor(item){
+        this.description = item;
+        this.date = item.valid_date;
+    }
 }
 
 server.listen(PORT,()=>{
